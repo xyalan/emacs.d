6e940a0dc1c201d0740dfdcf57516295b323a543
Alan <alanlhy@gmail.com>
2015-01-26 23:16:54 +0800

    added automatic fmt go code

--- init.el
+++ init.el
@@ -71,6 +71,7 @@
 ;;(require 'company)
 ;;(require 'company-go)
 ;;only have company-mode enabled in go-mode
+(add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook 'company-mode)
 (add-hook 'go-mode-hook (lambda ()
                           (set (make-local-variable 'company-backends) '(company-go))

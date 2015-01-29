38439078e1203c465bd86f8fa32c01b3352e3288
Alan <alanlhy@gmail.com>
2015-01-30 00:00:41 +0800

    added ace-jump-buffer

--- elisp/load-acejump.el
+++ elisp/load-acejump.el
@@ -2,4 +2,8 @@
 (global-set-key (kbd "C-;") 'ace-jump-mode)
 (global-set-key (kbd "C-:") 'ace-jump-word-mode)
 
+(require-package 'ace-jump-buffer)
+(global-set-key (kbd "C-,") 'ace-jump-buffer)
+(global-set-key (kbd "C-.") 'ace-jump-buffer-other-window)
+
 (provide 'load-acejump)

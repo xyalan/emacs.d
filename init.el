95d77ae003bd12dc043b30e85c80f36b1ea2f7d6
Alan <alanlhy@gmail.com>
2015-02-04 17:17:20 +0800

    deleted fullsize, added helm+ido

--- init.el
+++ init.el
@@ -17,25 +17,18 @@
 (require 'load-utils)
 (require 'load-ido)
 (require 'init-helm)
-;;startup in maximized mode
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(markdown-command "/usr/local/bin/pandoc"))
 
 ;;------------------------------------------------------------------
 ;;configuration of color,use solarized
 ;;------------------------------------------------------------------
+;;(require-package 'solarized-theme)
 ;;(defun solarized-init ()
-;;  (load-theme 'solarized-dark)
+;;  (load-theme 'solarized-light t)
 ;;)
 ;;(add-hook 'after-init-hook 'solarized-init)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized")
-;;(set-frame-parameter nil 'background-mode 'dark)
-(load-theme 'solarized t)
+(set-frame-parameter nil 'background-mode 'dark)
+(load-theme 'solarized)
 
 ;;If necessary, add JDK_HOME or JAVA_HOME to the environment
 ;;(setenv "JDK_HOME" "/path/to/jdk")
@@ -66,3 +59,17 @@
 (require 'workgroups)
 (setq wg-prefix-key (kbd "C-c w"))
 (workgroups-mode 1)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))))

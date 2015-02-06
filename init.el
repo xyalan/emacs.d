92d6e0e911d374194eca1b40dc8b112820950800
Alan <alanlhy@gmail.com>
2015-02-07 00:32:46 +0800

    delete useless variable

--- init.el
+++ init.el
@@ -3,28 +3,12 @@
 
 ;;set global path
 (add-to-list 'load-path "~/.emacs.d/elisp")
-(setq *macbook-pro-support-enabled* t)
+
 ;;----------------------------------------------------------------------------
 ;; Which functionality to enable (use t or nil for true and false)
 ;;----------------------------------------------------------------------------
-(setq *macbook-pro-support-enabled* t)
 (setq *is-a-mac* (eq system-type 'darwin))
-(setq *is-carbon-emacs* (and *is-a-mac* (eq window-system 'mac)))
 (setq *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
-(setq *win32* (eq system-type 'windows-nt) )
-(setq *cygwin* (eq system-type 'cygwin) )
-(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
-(setq *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)) )
-(setq *linux-x* (and window-system *linux*) )
-(setq *xemacs* (featurep 'xemacs) )
-(setq *emacs23* (and (not *xemacs*) (or (>= emacs-major-version 23))) )
-(setq *emacs24* (and (not *xemacs*) (or (>= emacs-major-version 24))) )
-(setq *no-memory* (cond
-		   (*is-a-mac*
-		    (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
-		   (*linux* nil)
-		   (t nil)
-		   ))
 
 (require 'load-elpa)
 (require 'load-ibuffer)

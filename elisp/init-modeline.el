ff7cf0e30bc08e5e88fcb0fb28061d8f3e4c8792
Alan <alanlhy@gmail.com>
2015-02-11 22:54:00 +0800

    set time format, added mode line theme

--- elisp/init-modeline.el
+++ elisp/init-modeline.el
@@ -1,3 +1,16 @@
+(require-package 'smart-mode-line)
+(sml/setup)
+(sml/apply-theme 'dark)
+
+(defvar lunaryorn-vc-mode-line
+  '(" " (:propertize
+         ;; Strip the backend name from the VC status information
+         (:eval (let ((backend (symbol-name (vc-backend (buffer-file-name)))))
+                  (substring vc-mode (+ (length backend) 2))))
+         face font-lock-variable-name-face))
+  "Mode line format for VC Mode.")
+(put 'lunaryorn-vc-mode-line 'risky-local-variable t)
+
 ;; @see http://emacs-fu.blogspot.com/2011/08/customizing-mode-line.html
 ;; But I need global-mode-string,
 ;; @see http://www.delorie.com/gnu/docs/elisp-manual-21/elisp_360.html
@@ -29,12 +42,13 @@
 
     '(:eval (propertize "%m" 'face nil
               'help-echo buffer-file-coding-system))
-    " "
+    "] "
 
 
+    "["
     ;; insert vs overwrite mode, input-method in a tooltip
     '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-              'face nil
+              'face font-lock-preprocessor-face
               'help-echo (concat "Buffer is in "
                            (if overwrite-mode "overwrite" "insert") " mode")))
 
@@ -52,7 +66,13 @@
     "] "
 
     ;;global-mode-string, org-timer-set-timer in org-mode need this
-    (propertize "%M" 'face nil)
+    ;;(propertize "%M" 'face nil)
+    '(:eval (propertize (format-time-string "%a %b %d %H:%M")
+              'help-echo
+              (concat (format-time-string "%c; ")
+                      (emacs-uptime "Uptime:%hh"))))
+
+    "%n"
 
     " --"
     ;; i don't want to see minor-modes; but if you want, uncomment this:
--- init.el
+++ init.el
@@ -1,3 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default))))
+
 ;;set global path
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
@@ -9,8 +18,8 @@
 (setq *win32* (eq system-type 'windows-nt) )
 
 (require 'cl)
-(require 'init-modeline)
 (require 'load-elpa)
+(require 'init-modeline)
 (require 'load-ibuffer)
 (require 'sql-indent)
 (require 'load-git)
@@ -24,6 +33,7 @@
 (require 'init-environment)
 (require 'init-windows)
 (require 'init-misc)
+(require 'vc-git)
 ;;------------------------------------------------------------------
 ;;configuration of color,use solarized
 ;;------------------------------------------------------------------
@@ -45,3 +55,10 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))

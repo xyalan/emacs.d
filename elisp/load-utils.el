94a1ab408d9e14899bf6a857b02be873b8d98b17
Alan <alanlhy@gmail.com>
2015-02-14 00:24:00 +0800

    delete shortcut of delete-backend-chars with C-h

--- elisp/load-utils.el
+++ elisp/load-utils.el
@@ -47,7 +47,7 @@
 
 (global-set-key (kbd "C-?") 'help-command)
 (global-set-key (kbd "M-?") 'mark-paragraph)
-(global-set-key (kbd "C-h") 'delete-backward-char)
+;;(global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-h") 'backward-kill-word)
 
 (require 'load-acejump)
